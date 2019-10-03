847f5c41246c454b7c74bfb2d3042981a2819318
Aries Beltran <ariesr.beltran@pccw.com>
2019-10-03 10:03:59 +0800

    Added support for specifying feature folder

--- bin/autokin-cli.js
+++ bin/autokin-cli.js
@@ -7,7 +7,7 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports.default = function ({ tags, formatter, junit, variables, time, html, ci }) {
+module.exports.default = function ({ specs, tags, formatter, junit, variables, time, html, ci }) {
     let formatterPath = path.resolve(__dirname, '../lib/formatter/autokin-formatter');
     let htmlFormatterPath = path.resolve(__dirname, '../lib/formatter/autokin-html-formatter');
 
@@ -15,7 +15,16 @@ module.exports.default = function ({ tags, formatter, junit, variables, time, ht
         fs.mkdirSync('reports/snapshots', { recursive: true });
     }
 
-    let cliOptions = ['', '', '--format=json:reports/autokin-report.json', '--format=' + formatterPath];
+    let cliOptions = ['', ''];
+    if (specs) cliOptions = cliOptions.concat([specs]);
+
+    cliOptions = cliOptions.concat([
+        '--format=json:reports/autokin-report.json',
+        '--format=' + formatterPath,
+        '--require=./lib/autokin-options.js',
+        '--require=./lib/autokin-rest-steps.js',
+        '--require=./lib/web/autokin-web.js']);
+        
     if (html)  { 
         const targetHtmlPath = typeof (html) == 'boolean' ? 'reports/autokin-result.html' : html;
         cliOptions = cliOptions.concat([`--format=node_modules/autokin-html-formatter:${targetHtmlPath}`]);
